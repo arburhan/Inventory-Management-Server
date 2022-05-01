8c5c9869e75dbf8a5edec27d58cc0d0ff806438f
arburhan <buthan190629@outlook.com>
2022-05-01 23:51:30 +0600

    add cluster code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -7,6 +8,17 @@ const app = express();
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+// cluster code 
+
+const uri = "mongodb+srv://inventoryAdmin:<password>@cluster0.1si1c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 
 app.get('/', (req, res) => {
