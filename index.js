e4a1da5d0d1e167235623239a44ee02dc69cf947
arburhan <buthan190629@outlook.com>
2022-05-05 17:01:00 +0600

    find by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -25,7 +25,13 @@ async function run() {
             const cursor = itemsCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
-        })
+        });
+        app.get('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemsCollection.findOne(query);
+            res.send(item);
+        });
     }
     finally {
 
